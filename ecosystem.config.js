89931fa80985f7786403f6041ee44dd12da181b1
sukriyatma <sukriyatmamuhammad@apps.ipb.ac.id>
2024-09-08 21:50:11 +0700

    fix: fixing pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,14 @@ module.exports = {
     apps: [
         {
             name: 'gt-auto-portal-service',
-            script: 'node_modules/.bin/ts-node',
-            args: 'src/main.ts',
-            cwd: '.',
-            watch: true,
-            ignore_watch: ['node_modules', 'dist']
-        }
-    ]
-}
+            script: 'dist/main.js',
+            instance: 1,
+            exec_mode: 'fork',
+                watch: true,
+                ignore_watch: ['node_modules', 'dist'],
+            env: {
+                NODE_ENV: 'production'
+            },
+        },
+    ],
+};
