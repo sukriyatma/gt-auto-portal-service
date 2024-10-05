c20a7052b3289e44534a54490f9ea28823b42f37
sukriyatma <sukriyatmamuhammad@apps.ipb.ac.id>
2024-10-05 16:00:44 +0700

    get-api-key interface

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { where } from 'sequelize';
 import { ApiResponseDto } from 'src/common/dto/api-response.dto';
+import { GetApiKeyResDto } from 'src/common/dto/get-apiKey-res.dto';
 import { UpdateFcmTokenDto } from 'src/common/dto/update-fcmtoken.dto';
 import { Users } from 'src/common/models/users.model';
 
@@ -26,5 +27,13 @@ export class UserService {
         );
 
         return ApiResponseDto.success(null);
+    }
+
+    async getApiKey(user: Users): Promise<ApiResponseDto<GetApiKeyResDto>> {
+        const response = new GetApiKeyResDto();
+        response.apiKey = user.apiKey;
+        console.log(user)
+
+        return ApiResponseDto.success(response);
     }
 }
